f6aafa598c55103b8a1cd728bcda66d1464d8436
Marzio <marziofarina@icloud.com>
2024-05-29 11:41:24 +0200

    adeguamenti visivi al codice alla pagine home

--- Eos/resources/views/home.blade.php
+++ Eos/resources/views/home.blade.php
@@ -4,9 +4,22 @@
             <h1>Login</h1>
             <form method="POST" action="{{route('login')}}">
                 @csrf
-                <input type="email" name="Email" id="email" placeholder=" Email">        
-                <input type="password" name="Password" id="password" placeholder=" Password">
-                <input type="submit" name="Accedi" id="accedi" value="Accedi">
+                <input 
+                    type="email" 
+                    name="Email" 
+                    id="email" 
+                    value="{{old('Email')}}"
+                    placeholder=" Email">                            
+                <input 
+                    type="password" 
+                    name="Password" 
+                    id="password" 
+                    placeholder=" Password">
+                <input 
+                    type="submit" 
+                    name="Accedi" 
+                    id="accedi" 
+                    value="Accedi">
             </form>
         </div>
     </div>
