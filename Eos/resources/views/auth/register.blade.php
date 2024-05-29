6bbbe266e1840ed5f014574f50ed2990732752f0
Marzio <marziofarina@icloud.com>
2024-05-29 11:31:24 +0200

    adeguamenti grafici alla pagina blade registrazione

--- Eos/resources/views/auth/register.blade.php
+++ Eos/resources/views/auth/register.blade.php
@@ -3,53 +3,55 @@
         <div class="register">
             <form method="POST" action="/register">
                 @csrf
-                <div class="mb-3">
-                    <h1>Registrati</h1>
-                    <input 
-                        type="text" 
-                        class="form-control" 
-                        name="name" 
-                        value="{{old('name')}}"
-                        placeholder="Username">
-                    @error('name')
-                        <div class="alert alert-danger" role="alert">
-                            {{$message}}
-                        </div>
-                    @enderror
+                <h1>Registrati</h1>
+                <div>
+                    <div class="mb-3">
+                        <input 
+                            type="text" 
+                            class="form-control" 
+                            name="name" 
+                            value="{{old('name')}}"
+                            placeholder="Username">
+                        @error('name')
+                            <div class="alert alert-danger" role="alert">
+                                {{$message}}
+                            </div>
+                        @enderror
+                    </div>
+                    <div class="mb-3">
+                        <input 
+                            type="email" 
+                            class="form-control" 
+                            name="email" 
+                            value="{{old('email')}}"
+                            placeholder="Email">
+                        @error('email')
+                            <div class="alert alert-danger" role="alert">
+                                {{$message}}
+                            </div>
+                        @enderror
+                    </div>
+                    <div class="mb-3">
+                        <input 
+                            type="password" 
+                            class="form-control" 
+                            name="password"
+                            placeholder="Password">
+                        @error('password')
+                            <div class="alert alert-danger" role="alert">
+                                {{$message}}
+                            </div>
+                        @enderror
+                    </div>
+                    <div class="mb-3">
+                        <input 
+                            type="password" 
+                            class="form-control" 
+                            name="password_confirmation"
+                            placeholder="Conferma password">
+                    </div>
                 </div>
-                <div class="mb-3">
-                    <input 
-                        type="email" 
-                        class="form-control" 
-                        name="email" 
-                        value="{{old('email')}}"
-                        placeholder="Email">
-                    @error('email')
-                        <div class="alert alert-danger" role="alert">
-                            {{$message}}
-                        </div>
-                    @enderror
-                </div>
-                <div class="mb-3">
-                    <input 
-                        type="password" 
-                        class="form-control" 
-                        name="password"
-                        placeholder="Password">
-                    @error('password')
-                        <div class="alert alert-danger" role="alert">
-                            {{$message}}
-                        </div>
-                    @enderror
-                </div>
-                <div class="mb-3">
-                    <input 
-                        type="password" 
-                        class="form-control" 
-                        name="password_confirmation"
-                        placeholder="Conferma password">
-                </div>
-                <button type="submit" id="accedi">Registrati</button>
+                <button type="submit" id="registrati">Registrati</button>
             </form>
         </div>        
     </div>
