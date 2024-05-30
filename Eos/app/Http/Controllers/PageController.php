0032c169d42ea1c60f263cec7d38e0a6fef9a23e
Marzio <marziofarina@icloud.com>
2024-05-30 14:37:36 +0200

    Aggiustamenti generici

--- Eos/app/Http/Controllers/PageController.php
+++ Eos/app/Http/Controllers/PageController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use function Ramsey\Uuid\v1;
+
 class PageController extends Controller
 {
     public function home () {
--- Eos/resources/views/home.blade.php
+++ Eos/resources/views/home.blade.php
@@ -1,26 +1,28 @@
 <x-layout>
-    <div class="homeLogin">
-        <div class="login">
-            <h1>Login</h1>
-            <form method="POST" action="{{route('Dashboard')}}">
-                @csrf
-                <input 
-                    type="email" 
-                    name="Email" 
-                    id="email" 
-                    value="{{old('Email')}}"
-                    placeholder=" Email">                            
-                <input 
-                    type="password" 
-                    name="Password" 
-                    id="password" 
-                    placeholder=" Password">
-                <input 
-                    type="submit" 
-                    name="Accedi" 
-                    id="accedi" 
-                    value="Accedi">
-            </form>
+    @guest
+        <div class="homeLogin">
+            <div class="login">
+                <h1>Login</h1>
+                <form method="POST" action="/login">
+                    @csrf
+                    <input type="email" name="email" id="email" value="{{old('email')}}" placeholder="Email">
+                    @error('email')
+                        <div class="alert alert-danger" role="alert">
+                            {{$message}}
+                        </div>
+                    @enderror                            
+                    <input type="password" name="password" id="password" placeholder="Password">
+                    @error('password')
+                        <div class="alert alert-danger" role="alert">
+                            {{$message}}
+                        </div>
+                    @enderror  
+                    <input type="submit" name="Accedi" id="accedi" value="Accedi">
+                </form>
+            </div>
         </div>
-    </div>
+    @endguest
+    @auth
+        <script>window.location = "/Dashboard";</script>
+    @endauth
 </x-layout>
