d4f4c98c100440738f6a4b5bdb7e85ef8cfdb4e2
Marzio <marziofarina@icloud.com>
2024-06-01 12:42:41 +0200

    reso pulito il foglio blade degli utenti

--- Eos/resources/views/components/Utenti/tabella.blade.php
+++ Eos/resources/views/components/Utenti/tabella.blade.php
@@ -1,28 +1,37 @@
-<div class="pt-2 ">
-    <table class="table table-striped table-hover table-bordered">
-        <thead>
-          <tr>
-            <th scope="col">ID</th>
-            <th scope="col">User</th>
-            <th scope="col">E-mail</th>
-            <th scope="col">Stato</th>
-            <th scope="col">Autorizzazioni</th>
-          </tr>
-        </thead>
-        <tbody class="table-group-divider">
-          <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>Otto</td>
-            <td>Otto</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td colspan="2">Larry the Bird</td>
-            <td>Otto</td>
-            <td>Otto</td>
-          </tr>
-        </tbody>
-    </table>
+<div class="container pt-4">
+    <div class="filtraUtenti">
+        <h5>Lista degli utenti</h5>
+        <form class="d-flex" role="search">
+            <input class="me-2" type="search">
+            <button class="btn btn-outline-success" type="submit">Filtra</button>
+        </form>
+    </div>
+    <div class="pt-2 ">
+        <table class="table table-striped table-hover table-bordered">
+            <thead>
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">User</th>
+                <th scope="col">E-mail</th>
+                <th scope="col">Stato</th>
+                <th scope="col">Autorizzazioni</th>
+            </tr>
+            </thead>
+            <tbody class="table-group-divider">
+            <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>Otto</td>
+                <td>Otto</td>
+            </tr>
+            <tr>
+                <th scope="row">3</th>
+                <td colspan="2">Larry the Bird</td>
+                <td>Otto</td>
+                <td>Otto</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
 </div>
