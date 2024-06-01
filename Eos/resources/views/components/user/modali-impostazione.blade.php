379f3d181cecdf85e3921bf9fdfbe274c0b31721
Marzio <marziofarina@icloud.com>
2024-06-01 20:43:19 +0200

    aggiunto il primo modale per l'edit dell'username ma ancora da completare

---
+++ Eos/resources/views/components/user/modali-impostazione.blade.php
@@ -0,0 +1,35 @@
+
+{{-- Modale per l'Edit dell'Username --}}
+
+<div class="modal" id="EditUsername" tabindex="-1">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Modifica dell'Username</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+            <div>
+                <p>
+                    L'Username attuale è :
+                </p>
+                <p>
+                    Nuovo Username :
+                </p>
+            </div>
+            <div >
+                <p id="DatoModale">
+                    {{$user->name}}
+                </p>
+                
+                <input type="text">
+            </div>
+            
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-warning" data-bs-dismiss="modal">Cancella</button>
+          <button type="button" class="btn btn-primary">Salva</button>
+        </div>
+      </div>
+    </div>
+</div>
