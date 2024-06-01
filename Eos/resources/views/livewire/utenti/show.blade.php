ab1e30bc0132eaab1b852e30317fa913a09f0ec3
Marzio <marziofarina@icloud.com>
2024-06-01 17:33:56 +0200

    eseguita una parte della parte grafica per lo show dell'utente

--- Eos/resources/views/livewire/utenti/show.blade.php
+++ Eos/resources/views/livewire/utenti/show.blade.php
@@ -1,3 +1,16 @@
-<div>
-    <p>prova</p>
+<div class="container mt-4">    
+    <div class="row">
+        <h5 class="text-center">Impostazioni di </h5>
+        <aside class="col-3 pagineUtente">
+            <p>Dati anagrafici</p>
+            <span class="separatoreCasisticheUtente"></span>
+            <p>Reset password</p>
+            <span class="separatoreCasisticheUtente"></span>
+            <p>Impostazioni</p>
+            <span class="separatoreCasisticheUtente"></span>
+        </aside>
+        <main class="col-9 dettagliUtente">
+            <p>impostazioni dell'utente</p>
+        </main>
+    </div>
 </div>
