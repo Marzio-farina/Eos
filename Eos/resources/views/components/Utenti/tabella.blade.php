35ae436be437f2039cda7965a0fc366133d64029
Marzio <marziofarina@icloud.com>
2024-06-01 12:05:44 +0200

    integrazioni grafiche alla tabella degli user

--- Eos/resources/views/components/Utenti/tabella.blade.php
+++ Eos/resources/views/components/Utenti/tabella.blade.php
@@ -1,26 +1,27 @@
-<div class="pt-2">
-    <table class="table table-striped table-hover border border-dark-subtle">
+<div class="pt-2 ">
+    <table class="table table-striped table-hover table-bordered">
         <thead>
           <tr>
             <th scope="col">ID</th>
             <th scope="col">User</th>
             <th scope="col">E-mail</th>
+            <th scope="col">Stato</th>
+            <th scope="col">Autorizzazioni</th>
           </tr>
         </thead>
-        <tbody>
+        <tbody class="table-group-divider">
           <tr>
             <th scope="row">1</th>
             <td>Mark</td>
             <td>Otto</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
+            <td>Otto</td>
+            <td>Otto</td>
           </tr>
           <tr>
             <th scope="row">3</th>
             <td colspan="2">Larry the Bird</td>
+            <td>Otto</td>
+            <td>Otto</td>
           </tr>
         </tbody>
     </table>
