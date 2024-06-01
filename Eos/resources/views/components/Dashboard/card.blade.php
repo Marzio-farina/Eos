3832ccfb3400e3c88c746d47b80be958ed50e8cd
Marzio <marziofarina@icloud.com>
2024-06-01 11:14:42 +0200

    risistemate le cartelle dei fogli di stile delle card, ora sono piu ordinate ancora

--- Eos/resources/views/components/Dashboard/card.blade.php
+++ Eos/resources/views/components/Dashboard/card.blade.php
@@ -1,13 +1,13 @@
 <div class="row">
     <div class="col-12 col-md-5 col-xl-3">
-        <div class="p-3 SottoContenitoreCardDashboard ContenitoreAcquisti">
+        <div class="p-3 SottoContenitoreCardDashboard ContenitoreColoreBlu">
             <a class="SottoContenitoreCardDashboardSopra" href="#">
-                <span class="ContenitoreIconaCardDashboard ContenitoreIconaAcquisti">
+                <span class="ContenitoreIconaCardDashboard ContenitoreIconaColoreBLu">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
                         <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l1.313 7h8.17l1.313-7zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
                     </svg>
                 </span>
-                <span class="ContenitoreDescrizioneIconaCardDashboard ContenitoreDescrizioneIconaAcquisti">
+                <span class="ContenitoreDescrizioneIconaCardDashboard ContenitoreDescrizioneColoreBlu">
                     Ordini
                 </span>
             </a>
@@ -25,7 +25,7 @@
             </div>
         </div>
     </div>
-    <div class="col-12 col-md-5 col-xl-3">
+    {{-- <div class="col-12 col-md-5 col-xl-3">
         <div class="p-3 SottoContenitoreCardDashboard ContenitoreReddito">
             <a class="SottoContenitoreCardDashboardSopra" href="#">
                 <span class="ContenitoreIconaCardDashboard ContenitoreIconaReddito">
@@ -101,6 +101,6 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </div> --}}
     </div>
 </div>
