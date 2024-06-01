cef795b17ec34c3cef5b9445b3d8917b9fe43168
Marzio <marziofarina@icloud.com>
2024-06-01 12:39:41 +0200

    rimosso una classe di bootstrap che dava fastidio

--- Eos/resources/views/Utenti.blade.php
+++ Eos/resources/views/Utenti.blade.php
@@ -2,7 +2,13 @@
 <x-layout>
     <x-navigazione/>
     <div class="container pt-4">
-        <h5>Lista degli utenti</h5>
+        <div class="filtraUtenti">
+            <h5>Lista degli utenti</h5>
+            <form class="d-flex" role="search">
+                <input class="me-2" type="search">
+                <button class="btn btn-outline-success" type="submit">Filtra</button>
+            </form>
+        </div>
         <x-Utenti.tabella/>
     </div>
 </x-layout>
