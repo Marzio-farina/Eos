4ca6310fc80bcbdcb1dbe1e18a568aa2bbb7e2ba
Marzio <marziofarina@icloud.com>
2024-06-13 16:13:09 +0200

    modificato il seed per campiClient in modo che mi crea in modo statico ogni volta anche varie commesse

--- Eos/database/seeders/CampiClientSeeder.php
+++ Eos/database/seeders/CampiClientSeeder.php
@@ -13,30 +13,53 @@ class CampiClientSeeder extends Seeder
      */
     public function run(): void
     {
-        $campiClient = [
-            'nomeCommittente' => 'Terna',
-            '01_Allegato_8' => false,
-            '02_Allegato_9' => false,
-            '03_Attestati_di_formazione' => false,
-            '04_Allegato_10' => false,
-            '05_Mezzi_Macchinari' => false,
-            '06_Attrezzature' => false,
-            '07_Nomine_Figure_Responsabilità' => false,
-            '08_POS_Allegati' => false,
-            '09_DVR' => false,
-            '10_Visura_CCIAA' => false,
-            '11_Dichiarazione_CCNL' => false,
-            '12_Iscrizione_Gestori_Ambientali' => false,
-            '13_Dichiarazione_Cave_Discariche' => false,
-            '14_SOA' => false,
-            '15_White_List' => false,
-            '16_Dichiarazione_art_14' => false,
-            '17_Procedura_Luoghi_Confinati' => false,
-            '18_Piano_Amianto' => false,
+        $campiClients = [
+            [
+                'nomeCommittente' => 'Terna',
+                '01_Allegato_8' => false,
+                '02_Allegato_9' => false,
+                '03_Attestati_di_formazione' => false,
+                '04_Allegato_10' => false,
+                '05_Mezzi_Macchinari' => false,
+                '06_Attrezzature' => false,
+                '07_Nomine_Figure_Responsabilità' => false,
+                '08_POS_Allegati' => false,
+                '09_DVR' => false,
+                '10_Visura_CCIAA' => false,
+                '11_Dichiarazione_CCNL' => false,
+                '12_Iscrizione_Gestori_Ambientali' => false,
+                '13_Dichiarazione_Cave_Discariche' => false,
+                '14_SOA' => false,
+                '15_White_List' => false,
+                '16_Dichiarazione_art_14' => false,
+                '17_Procedura_Luoghi_Confinati' => false,
+                '18_Piano_Amianto' => false,
+            ],
+            [
+                'nomeCommittente' => 'Areti',
+                '01_Allegato_8' => false,
+                '02_Allegato_9' => false,
+                '03_Attestati_di_formazione' => false,
+                '04_Allegato_10' => false,
+                '05_Mezzi_Macchinari' => false,
+                '06_Attrezzature' => false,
+                '07_Nomine_Figure_Responsabilità' => false,
+                '08_POS_Allegati' => false,
+                '09_DVR' => false,
+                '10_Visura_CCIAA' => false,
+                '11_Dichiarazione_CCNL' => false,
+                '12_Iscrizione_Gestori_Ambientali' => false,
+                '13_Dichiarazione_Cave_Discariche' => false,
+                '14_SOA' => false,
+                '15_White_List' => false,
+                '16_Dichiarazione_art_14' => false,
+                '17_Procedura_Luoghi_Confinati' => false,
+                '18_Piano_Amianto' => false,
+            ],
         ];
 
-        // foreach($campiClients as $campiClient){
+        foreach($campiClients as $campiClient){
             campiClient::create($campiClient);
-        // }
+        }
     }
 }
