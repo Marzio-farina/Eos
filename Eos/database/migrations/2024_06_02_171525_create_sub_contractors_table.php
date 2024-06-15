85203656edc67c77c743faef464f6355c874ad23
Marzio <marziofarina@icloud.com>
2024-06-15 17:39:50 +0200

    mi son fermato, alla ripresa devo creare la tabella sub_contractors, il modello, la factory e proseguire con il collegamento della chiave secondaria di orders

---
+++ Eos/database/migrations/2024_06_02_171525_create_sub_contractors_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sub_contractors', function (Blueprint $table) {
+            $table->unsignedTinyInteger('id')->autoIncrement()->primary();
+            $table->string('nome',150)->nullable();
+            $table->boolean('DVR')->default(false);
+            $table->boolean('CCIA')->default(false);
+            $table->boolean('VCCIA')->default(false);
+            $table->boolean('nominaMedicoCompetente')->default(false);
+            $table->unsignedTinyInteger('POS_id')->nullable();
+            $table->boolean('nominaRLST')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sub_contractors');
+    }
+};
